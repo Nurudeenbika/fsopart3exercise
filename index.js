d291cc57bbcf9a752f73cc084cb5c63ccf2eb326
Hassan Nurudeen <nurudeenhassan862@gmail.com>
2024-08-17 15:45:35 +0100

    feat: updated to 3.18* solution

--- index.js
+++ index.js
@@ -47,23 +47,12 @@ const personSchema = new mongoose.Schema({
   number: String,
 })
 
-
-
-
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.random(...persons.map(p => Number(p.id)))
-    : 0
-  return String(maxId + 7)
-}
-
 app.post('/api/persons', (request, response) => {
-  body = request.body
+  body = request.body;
 
  
   if (!body.name || !body.number === undefined) {
@@ -80,40 +69,74 @@ app.post('/api/persons', (request, response) => {
   } 
   const person = new Person({
     name: body.name,
-    number: String(body.number)
+    number: body.number
   })
   person.save().then(savePerson => {
-    response.json(savePerson)
+    response.json(savePerson);
   })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error));
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    response.json(persons)
+    response.json(persons);
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end();
+    }
   })
+  .catch(error => next(error));
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
-
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error))
 })
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
+    returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id
     delete returnedObject.__v
   }
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error);
+}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello world!</h1>')
@@ -124,7 +147,10 @@ app.get('/info', (request, response) => {
   response.send(`Phonebook has info for 2 people <p> ${currentDate}</p>`)
 })
 
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 })

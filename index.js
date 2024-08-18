afee3d0f39af597ada99886a80be0c9b8a2d47e8
Hassan Nurudeen <nurudeenhassan862@gmail.com>
2024-08-18 14:43:17 +0100

    fix: fixes on errorHandler

--- index.js
+++ index.js
@@ -51,11 +51,11 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   body = request.body;
 
  
-  if (!body.name || !body.number === undefined) {
+  if (body.name || body.number === undefined) {
     return response.status(400).json({
       error: 'name or number missing'
     })
@@ -74,17 +74,19 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savePerson => {
     response.json(savePerson);
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
   const person = {
     name: body.name,
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, 
+    { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson);
     })
@@ -133,6 +135,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error);
